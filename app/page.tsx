7db00350135c3640a633469b4611f0065f04c05d
venkatesh-pro <programervenkatesh@gmail.com>
2026-03-12 19:54:19 +0530

    form added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Navbar from '@/components/Navbar';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Slider = () => {
   return <img src={'/banner1.jpg'} className="h-full w-full"></img>;
@@ -8,7 +11,25 @@ const Slider = () => {
 const formatPrice = (currency: string, value: number) => {
   return `${currency === 'dollar' && '$'}${new Intl.NumberFormat().format(value)}`;
 };
+
+const InputField = ({ type, placeholder, register, label, isRequired, errors }) => {
+  return (
+    <input
+      className={`h-[56px] w-full py-[20px] px-[24px] rounded-[100px] border-[1px] border-[#C4C4C4] focus:outline-[1px] focus:outline-[#171A20] focus:border-[#171A20] placeholder:text-[#5C5E62] text-[17px] font-[400] placeholder:text-[17px] placeholder:font-[400] tracking-[1%] leading-[100%] ${
+        errors?.[label] &&
+        '!border-[#B74134] !placeholder-[#B74134] !bg-[#ED4E3B1A] active:!border-[#B74134] focus:!border-[#B74134] focus:!outline-none '
+      }`}
+      type={type}
+      placeholder={placeholder}
+      {...register(label, {
+        required: isRequired ? 'Required' : false,
+      })}
+    />
+  );
+};
 const Configurator = () => {
+  const [isContinue, setIsContinue] = useState(true);
+
   const data = {
     sizes: [
       {
@@ -118,6 +139,15 @@ const Configurator = () => {
       },
     ],
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => console.log(data);
+
   return (
     <div className="mx-[48px]">
       <div>
@@ -310,13 +340,151 @@ const Configurator = () => {
           requirements.
         </p>
 
-        <button className="mt-[32px] w-full h-[56px] bg-[#171A20] rounded-[100px] text-white text-[17px] font-[400] tracking-[1%] leading-[100%]">
+        <button
+          onClick={() => {
+            setIsContinue(true);
+          }}
+          className="cursor-pointer mt-[32px] w-full h-[56px] bg-[#171A20] rounded-[100px] text-white text-[17px] font-[400] tracking-[1%] leading-[100%]"
+        >
           Continue
         </button>
       </div>
 
       <div className="h-[80px]"></div>
-      {/*  */}
+      {/* submit details */}
+      {isContinue && (
+        <div>
+          <p className="text-[#171A20] text-[21px] font-[500] tracking-[1%] leading-[100%]">
+            Submit details to confirm price and delivery availability.
+          </p>
+          <p className="text-[#5C5E62] text-[14px] font-[400] tracking-[1.5%] leading-[100%] mt-[8px]">
+            Receive a formal proposal and delivery timeline for your project.
+          </p>
+
+          <div className="mt-[24px] flex flex-col gap-[16px]">
+            <div>
+              <InputField
+                isRequired={true}
+                label={'company'}
+                placeholder={'Company'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.company && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.company.message as string}
+                </p>
+              )}
+            </div>
+            <div>
+              <InputField
+                isRequired={true}
+                label={'name'}
+                placeholder={'Name'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.name && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.name.message as string}
+                </p>
+              )}
+            </div>
+            <div>
+              <InputField
+                isRequired={true}
+                label={'siteAddress'}
+                placeholder={'Site Address'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.siteAddress && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.siteAddress.message as string}
+                </p>
+              )}
+            </div>
+            <div>
+              <InputField
+                isRequired={true}
+                label={'postalCode'}
+                placeholder={'Postal Code'}
+                register={register}
+                type={'number'}
+                errors={errors}
+              />
+              {errors.postalCode && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.postalCode.message as string}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <InputField
+                isRequired={true}
+                label={'projectType'}
+                placeholder={'Project Type'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.projectType && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.projectType.message as string}
+                </p>
+              )}
+            </div>
+            <div>
+              <InputField
+                isRequired={true}
+                label={'emailAddress'}
+                placeholder={'Email Address'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.emailAddress && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.emailAddress.message as string}
+                </p>
+              )}
+            </div>
+            <div>
+              <InputField
+                isRequired={true}
+                label={'phoneNumber'}
+                placeholder={'Phone Number'}
+                register={register}
+                type={'text'}
+                errors={errors}
+              />
+              {errors.phoneNumber && (
+                <p className="text-[14px] mt-[10px] tracking-[1.5%] leading-[100%] font-[400] text-[#B74134]">
+                  {errors.phoneNumber.message as string}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <p className="text-[14px] mt-[24px] tracking-[1.5%] leading-[100%] font-[400] text-[#171A20]">
+            Estimated production lead time: 3-6 weeks from confirmed order.
+          </p>
+          <p className="text-[14px] mt-[16px] tracking-[1.5%] leading-[100%] font-[400] text-[#5C5E62]">
+            Pricing shown is indicative based on selected configuration. Delivery, installation and
+            permits may vary by site and location.
+          </p>
+          <button
+            onClick={handleSubmit(onSubmit)}
+            className="cursor-pointer mt-[32px] w-full h-[56px] bg-[#171A20] rounded-[100px] text-white text-[17px] font-[400] tracking-[1%] leading-[100%]"
+          >
+            Request Proposal
+          </button>
+        </div>
+      )}
     </div>
   );
 };
@@ -331,6 +499,7 @@ const page = () => {
       </div>
       <div className="mt-[140px]">
         <Configurator />
+        <div className="h-[500px] "></div>
       </div>
     </div>
   );
